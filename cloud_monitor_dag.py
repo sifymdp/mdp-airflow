8ece32b84b4e69cec84c6fe0eed0076bf095a057
sifymdp <155604819+sifymdp@users.noreply.github.com>
2024-02-06 21:23:21 +0530

    Update cloud_monitor_dag.py

--- cloud_monitor_dag.py
+++ cloud_monitor_dag.py
@@ -18,20 +18,38 @@ dag = DAG(
 
 
 def test_connection_task(**kwargs):
-    try:
-        # Set up Google Cloud Monitoring client
-        credentials = service_account.Credentials.from_service_account_file(
-            '/opt/airflow/secrets/google_cloud_default.json',
-            scopes=['https://www.googleapis.com/auth/monitoring.read']
-        )
-        client = monitoring_v3.MetricServiceClient(credentials=credentials)
-        project_id = 'airflow-8080'
-        client.list_monitored_resource_descriptors(
-            name=f'projects/{project_id}')
-        print("Connection to Google Cloud Monitoring API successful.")
-    except Exception as e:
-        print(f"Error connecting to Google Cloud Monitoring API: {e}")
+    # try:
+    #     # Set up Google Cloud Monitoring client
+    #     credentials = service_account.Credentials.from_service_account_file(
+    #         '/opt/airflow/secrets/google_cloud_default.json',
+    #         scopes=['https://www.googleapis.com/auth/monitoring.read']
+    #     )
+    #     client = monitoring_v3.MetricServiceClient(credentials=credentials)
+    #     project_id = 'airflow-8080'
+    #     client.list_monitored_resource_descriptors(
+    #         name=f'projects/{project_id}')
+    #     print("Connection to Google Cloud Monitoring API successful.")
+    # except Exception as e:
+    #     print(f"Error connecting to Google Cloud Monitoring API: {e}")
+      try:
+        # Specify the path to your service account key file
+        key_file_path = '/opt/airflow/secrets/google_cloud_default.json'
 
+        # Initialize the service account credentials
+        creds = service_account.Credentials.from_service_account_file(
+            key_file_path)
+
+        # Check if the credentials are valid
+        if not isinstance(creds, credentials.Credentials):
+            print("Google Cloud credentials are invalid.")
+            return False
+
+        print("Connection to Google Cloud successful.")
+        return True
+
+    except Exception as e:
+        print(f"Error connecting to Google Cloud: {e}")
+        return False
 
 test_connection = PythonOperator(
     task_id='test_connection',
