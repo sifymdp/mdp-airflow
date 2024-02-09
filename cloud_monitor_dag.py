8856fefe8fd2b90adcf9c8714a8aec8047c788a4
sifymdp <155604819+sifymdp@users.noreply.github.com>
2024-02-09 13:12:15 +0530

    Update cloud_monitor_dag.py

--- cloud_monitor_dag.py
+++ cloud_monitor_dag.py
@@ -14,7 +14,7 @@ default_args = {
     'start_date': datetime.today(),
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
-    'email': ['msg.smdp@gmail.com', 'duraisiva1905@gmail.com', 'parukrish21@gmail.com'],
+    'email': ['msg.smdp@gmail.com', 'managementairflow@gmail.com'],
     'email_on_failure': True,
     'email_on_retry': True
 }
