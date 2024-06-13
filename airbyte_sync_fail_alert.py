a9c5436f477e9d91d3c11f379703eb1960663274
sifymdp <155604819+sifymdp@users.noreply.github.com>
2024-06-13 12:18:30 +0530

    Update airbyte_sync_fail_alert.py

--- airbyte_sync_fail_alert.py
+++ airbyte_sync_fail_alert.py
@@ -48,35 +48,38 @@ def query_airbyte_logs():
 def send_email_alert(**kwargs):
     res= kwargs.get('ti').xcom_pull(task_ids='query_airbyte_sync_failure')
     res1=res[0]
-    print("resss::",res1)
-    subject = "Latest Airbyte Sync Status:"
-    body=f"""<table style="font-family: arial, sans-serif; border-collapse: collapse; width: 100%;">
-    <tr>
-        <th style="border: 1px solid #000000; text-align: left; padding: 8px;">Connection Name</th>
-        <th style="border: 1px solid #000000; text-align: left; padding: 8px;">Job Id</th>
-        <th style="border: 1px solid #000000; text-align: left; padding: 8px;">Config Type</th>
-        <th style="border: 1px solid #000000; text-align: left; padding: 8px;">Sync Status</th>
-    </tr>
-    <tr>
-        <td style="border: 1px solid #000000; text-align: left; padding: 8px;">{res1[0]}</td>
-        <td style="border: 1px solid #000000; text-align: left; padding: 8px;">{res1[1]}</td>
-        <td style="border: 1px solid #000000; text-align: left; padding: 8px;">{res1[2]}</td>
-        <td style="border: 1px solid #000000; text-align: left; padding: 8px;">{res1[3]}</td>
-
-        
-    </tr>
-</table>
-
-"""
-    to = ['managementairflow@gmail.com','saisushmitha.rama@sifycorp.com']
-
-    return EmailOperator(
-        task_id='send_email',
-        to=to,
-        subject=subject,
-        html_content=body,
-        dag=dag,
-    ).execute(context=None)
+    if res1[3]=="failed" or res1[3]=="Failed":
+        print("resss::",res1)
+        subject = "Latest Airbyte Sync Status:"
+        body=f"""<table style="font-family: arial, sans-serif; border-collapse: collapse; width: 100%;">
+        <tr>
+            <th style="border: 1px solid #000000; text-align: left; padding: 8px;">Connection Name</th>
+            <th style="border: 1px solid #000000; text-align: left; padding: 8px;">Job Id</th>
+            <th style="border: 1px solid #000000; text-align: left; padding: 8px;">Config Type</th>
+            <th style="border: 1px solid #000000; text-align: left; padding: 8px;">Sync Status</th>
+        </tr>
+        <tr>
+            <td style="border: 1px solid #000000; text-align: left; padding: 8px;">{res1[0]}</td>
+            <td style="border: 1px solid #000000; text-align: left; padding: 8px;">{res1[1]}</td>
+            <td style="border: 1px solid #000000; text-align: left; padding: 8px;">{res1[2]}</td>
+            <td style="border: 1px solid #000000; text-align: left; padding: 8px;">{res1[3]}</td>
+    
+            
+        </tr>
+    </table>
+    
+    """
+        to = ['managementairflow@gmail.com','saisushmitha.rama@sifycorp.com']
+    
+        return EmailOperator(
+            task_id='send_email',
+            to=to,
+            subject=subject,
+            html_content=body,
+            dag=dag,
+        ).execute(context=None)
+    else:
+        print("No failed syncs found")
 
 task_query = PythonOperator(
     task_id='query_airbyte_sync_failure',
