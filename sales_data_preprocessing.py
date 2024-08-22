d2773050382546cf4af37ee5db7ee573aec98830
sifymdp <155604819+sifymdp@users.noreply.github.com>
2024-08-22 14:57:14 +0530

    Update sales_data_preprocessing.py

--- sales_data_preprocessing.py
+++ sales_data_preprocessing.py
@@ -11,8 +11,8 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    # 'retries': 1,
+    # 'retry_delay': timedelta(minutes=5),
 }
  
 # Step-3: Define DAG
